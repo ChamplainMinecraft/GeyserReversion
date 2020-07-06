5f5ef6657e4b071202fe07ccc482a6dfb581925b
bundabrg <bundabrg@grieve.com.au>
2020-07-06 19:15:52 +0800

    Start of Login flow

--- src/main/java/au/com/grieve/edusupport/EduSupportPlugin.java
+++ src/main/java/au/com/grieve/edusupport/EduSupportPlugin.java
@@ -18,15 +18,25 @@
 
 package au.com.grieve.edusupport;
 
+import au.com.grieve.edusupport.utils.MCEELoginEncryptionUtils;
+import au.com.grieve.edusupport.utils.TokenManager;
+import com.nukkitx.protocol.bedrock.packet.LoginPacket;
+import com.nukkitx.protocol.bedrock.packet.PlayStatusPacket;
+import com.nukkitx.protocol.bedrock.packet.ResourcePacksInfoPacket;
 import com.nukkitx.protocol.bedrock.v363.Bedrock_v363;
+import lombok.Getter;
+import org.geysermc.connector.GeyserConnector;
 import org.geysermc.connector.event.annotations.Event;
 import org.geysermc.connector.event.events.BedrockCodecRegistryEvent;
+import org.geysermc.connector.event.events.BedrockPongEvent;
 import org.geysermc.connector.event.events.PluginDisableEvent;
 import org.geysermc.connector.event.events.PluginEnableEvent;
+import org.geysermc.connector.event.events.UpstreamPacketReceiveEvent;
 import org.geysermc.connector.plugin.GeyserPlugin;
 import org.geysermc.connector.plugin.PluginClassLoader;
 import org.geysermc.connector.plugin.PluginManager;
 import org.geysermc.connector.plugin.annotations.Plugin;
+import org.geysermc.connector.utils.LanguageUtils;
 
 @Plugin(
         name = "EduSupport",
@@ -34,15 +44,50 @@ import org.geysermc.connector.plugin.annotations.Plugin;
         authors = {"Bundabrg"},
         description = "Provides protocol support for Minecraft Educational Edition"
 )
+@Getter
 public class EduSupportPlugin extends GeyserPlugin {
+    @Getter
+    public static EduSupportPlugin instance;
+
+    private final TokenManager tokenManager;
 
     public EduSupportPlugin(PluginManager pluginManager, PluginClassLoader pluginClassLoader) {
         super(pluginManager, pluginClassLoader);
+
+        instance = this;
+        tokenManager = new TokenManager();
     }
 
     @Event
     public void onCodecRegistry(BedrockCodecRegistryEvent event) {
         event.setCodec(Bedrock_v363.V363_CODEC);
+    }
+
+    @Event
+    public void onBedrockPong(BedrockPongEvent event) {
+        event.getPong().setEdition("MCEE");
+    }
+
+    @Event(filter = LoginPacket.class)
+    public void onLoginPacket(UpstreamPacketReceiveEvent<LoginPacket> event) {
+        event.setCancelled(true);
+
+        if (event.getPacket().getProtocolVersion() > GeyserConnector.BEDROCK_PACKET_CODEC.getProtocolVersion()) {
+            event.getSession().disconnect(LanguageUtils.getPlayerLocaleString("geyser.network.outdated.server", event.getSession().getClientData().getLanguageCode(), GeyserConnector.BEDROCK_PACKET_CODEC.getMinecraftVersion()));
+            return;
+        } else if (event.getPacket().getProtocolVersion() < GeyserConnector.BEDROCK_PACKET_CODEC.getProtocolVersion()) {
+            event.getSession().disconnect(LanguageUtils.getPlayerLocaleString("geyser.network.outdated.client", event.getSession().getClientData().getLanguageCode(), GeyserConnector.BEDROCK_PACKET_CODEC.getMinecraftVersion()));
+            return;
+        }
+
+        MCEELoginEncryptionUtils.encryptPlayerConnection(getConnector(), event.getSession(), event.getPacket());
+
+        PlayStatusPacket playStatus = new PlayStatusPacket();
+        playStatus.setStatus(PlayStatusPacket.Status.LOGIN_SUCCESS);
+        event.getSession().sendUpstreamPacket(playStatus);
+
+        ResourcePacksInfoPacket resourcePacksInfo = new ResourcePacksInfoPacket();
+        event.getSession().sendUpstreamPacket(resourcePacksInfo);
     }
 
     @Event
---
+++ src/main/java/au/com/grieve/edusupport/utils/MCEELoginEncryptionUtils.java
@@ -0,0 +1,172 @@
+/*
+ * EduSupport - Minecraft Educational Support for Geyser
+ * Copyright (C) 2020 EduSupport Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package au.com.grieve.edusupport.utils;
+
+import au.com.grieve.edusupport.EduSupportPlugin;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSObject;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import com.nukkitx.protocol.bedrock.packet.LoginPacket;
+import com.nukkitx.protocol.bedrock.packet.ServerToClientHandshakePacket;
+import com.nukkitx.protocol.bedrock.util.EncryptionUtils;
+import org.geysermc.connector.GeyserConnector;
+import org.geysermc.connector.network.session.GeyserSession;
+import org.geysermc.connector.network.session.auth.AuthData;
+import org.geysermc.connector.network.session.auth.BedrockClientData;
+import org.geysermc.connector.utils.LoginEncryptionUtils;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PublicKey;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECGenParameterSpec;
+import java.util.Base64;
+import java.util.UUID;
+
+/**
+ * Provides an encrypted Login following the MCEE specifications
+ * <p>
+ * Much of this is copied from LoginEncryptUtils
+ */
+public class MCEELoginEncryptionUtils {
+    private static final ObjectMapper JSON_MAPPER = new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+    private static boolean validateChainData(JsonNode data) {
+        Method validateChainData;
+        try {
+            validateChainData = LoginEncryptionUtils.class.getDeclaredMethod("validateChainData", JsonNode.class);
+            validateChainData.setAccessible(true);
+            return (boolean) validateChainData.invoke(null, data);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void encryptPlayerConnection(GeyserConnector connector, GeyserSession session, LoginPacket loginPacket) {
+        JsonNode certData;
+        try {
+            certData = JSON_MAPPER.readTree(loginPacket.getChainData().toByteArray());
+        } catch (IOException ex) {
+            throw new RuntimeException("Certificate JSON can not be read.");
+        }
+
+        JsonNode certChainData = certData.get("chain");
+        if (certChainData.getNodeType() != JsonNodeType.ARRAY) {
+            throw new RuntimeException("Certificate data is not valid");
+        }
+
+        encryptConnectionWithCert(connector, session, loginPacket.getSkinData().toString(), certChainData);
+    }
+
+    private static void encryptConnectionWithCert(GeyserConnector connector, GeyserSession session, String clientData, JsonNode certChainData) {
+        try {
+            boolean validChain = validateChainData(certChainData);
+
+            connector.getLogger().debug(String.format("Is player data valid? %s", validChain));
+
+            JWSObject jwt = JWSObject.parse(certChainData.get(certChainData.size() - 1).asText());
+            JsonNode payload = JSON_MAPPER.readTree(jwt.getPayload().toBytes());
+
+            if (payload.get("extraData").getNodeType() != JsonNodeType.OBJECT) {
+                throw new RuntimeException("AuthData was not found!");
+            }
+
+            JsonNode extraData = payload.get("extraData");
+            session.setAuthenticationData(new AuthData(
+                    extraData.get("displayName").asText(),
+                    UUID.fromString(extraData.get("identity").asText()),
+                    extraData.get("XUID").asText()
+            ));
+
+            if (payload.get("identityPublicKey").getNodeType() != JsonNodeType.STRING) {
+                throw new RuntimeException("Identity Public Key was not found!");
+            }
+
+            ECPublicKey identityPublicKey = EncryptionUtils.generateKey(payload.get("identityPublicKey").textValue());
+            JWSObject clientJwt = JWSObject.parse(clientData);
+            EncryptionUtils.verifyJwt(clientJwt, identityPublicKey);
+
+            JsonNode jsonData = JSON_MAPPER.readTree(clientJwt.getPayload().toBytes());
+
+            session.setClientData(JSON_MAPPER.convertValue(jsonData, BedrockClientData.class));
+
+            if (EncryptionUtils.canUseEncryption()) {
+                startEncryptionHandshake(session, identityPublicKey, jsonData.get("TenantId").asText());
+            }
+        } catch (Exception ex) {
+            session.disconnect("disconnectionScreen.internalError.cantConnect");
+            throw new RuntimeException("Unable to complete login", ex);
+        }
+    }
+
+    protected static JWSObject createHandshakeJwt(KeyPair serverKeyPair, byte[] token, String signedToken) throws JOSEException {
+        URI x5u = URI.create(Base64.getEncoder().encodeToString(serverKeyPair.getPublic().getEncoded()));
+
+        JWTClaimsSet.Builder claimsBuilder = new JWTClaimsSet.Builder();
+        claimsBuilder.claim("salt", Base64.getEncoder().encodeToString(token));
+
+        if (signedToken != null && !signedToken.isEmpty()) {
+            claimsBuilder.claim("signedToken", signedToken);
+        }
+
+        SignedJWT jwt = new SignedJWT((new com.nimbusds.jose.JWSHeader.Builder(JWSAlgorithm.ES384)).x509CertURL(x5u).build(),
+                claimsBuilder.build());
+        EncryptionUtils.signJwt(jwt, (ECPrivateKey) serverKeyPair.getPrivate());
+        return jwt;
+    }
+
+    public static void startEncryptionHandshake(GeyserSession session, PublicKey key, String tenantId) throws Exception {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("EC");
+        generator.initialize(new ECGenParameterSpec("secp384r1"));
+        KeyPair serverKeyPair = generator.generateKeyPair();
+
+        byte[] token = EncryptionUtils.generateRandomToken();
+        SecretKey encryptionKey = EncryptionUtils.getSecretKey(serverKeyPair.getPrivate(), key, token);
+        session.getUpstream().getSession().enableEncryption(encryptionKey);
+
+        // Get signedToken for connecting client
+        String signedToken = null;
+        if (tenantId != null) {
+            if (EduSupportPlugin.getInstance().getTokenManager().getTokenMap().containsKey(tenantId)) {
+                // This may block if it needs to be refreshed
+                signedToken = EduSupportPlugin.getInstance().getTokenManager().getTokenMap().get(tenantId).getSignedToken();
+            } else {
+                session.getConnector().getLogger().warning("Unknown Tenant tried to connect: " + tenantId);
+            }
+        }
+
+        ServerToClientHandshakePacket packet = new ServerToClientHandshakePacket();
+        packet.setJwt(createHandshakeJwt(serverKeyPair, token, signedToken).serialize());
+        session.sendUpstreamPacketImmediately(packet);
+    }
+
+}
